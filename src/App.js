9a5bf0c9fb5e25f9c8ca5b53ca68f50c7a01909b
keroken <kenichi.shinagawa@gmail.com>
2020-05-18 01:40:39 +0900

    basic cube rotation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react';
+/** @jsx jsx **/
+import { css, jsx, Global } from '@emotion/core';
+import emotionReset from 'emotion-reset';
+import { Work } from './work';
 
-function App() {
-  return <h1>three-fiber</h1>;
+const globalStyles = css`
+  ${emotionReset}
+  *, *::after, *::before {
+    box-sizing: border-box;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    font-smoothing: antialiased;
+  }
+`;
 
-}
+const App = () => {
+  return (
+    <div>
+      <Global styles={globalStyles} />
+      <Work />
+    </div>
+  );
+};
 
 export default App;
---
+++ src/work.js
@@ -0,0 +1,40 @@
+/** @jsx jsx **/
+import { useRef } from 'react';
+import { Canvas, useFrame } from 'react-three-fiber';
+import { css, jsx } from '@emotion/core';
+
+const theme = css`
+  width: 100vw;
+  height: 100vh;
+  background-color: #000;
+`;
+
+const Thing = () => {
+  const ref = useRef();
+  useFrame(() => {
+    ref.current.rotation.y += 0.01;
+    ref.current.rotation.z += 0.01;
+  });
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}
+    >
+      <boxGeometry attach='geometry' args={[400, 400, 400]} />
+      <meshNormalMaterial
+        attach='material'
+      />
+    </mesh>
+  );
+};
+
+export const Work = () => (
+  <div css={theme}>
+    <Canvas camera={{ position: [0, 0, 1000] }}>
+      <Thing />
+    </Canvas>
+  </div>
+);
+
